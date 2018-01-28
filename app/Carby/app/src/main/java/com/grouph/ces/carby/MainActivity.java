00b0a573ab0042aaa58d6cc9ebfd55be48c50e01
George Cassels <george.cassels@gmail.com>
2018-01-28 16:32:25 +0000

    Barcode scanning sample code incorporated into app

--- app/Carby/app/src/main/java/com/grouph/ces/carby/MainActivity.java
+++ app/Carby/app/src/main/java/com/grouph/ces/carby/MainActivity.java
@@ -1,5 +1,6 @@
 package com.grouph.ces.carby;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,6 +82,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_barcode) {
+            Intent myIntent1 = new Intent(this, MainBarcodeActivity.class);
+            startActivity(myIntent1);
             // Handle the camera action
         } else if (id == R.id.nav_ocr) {
 
