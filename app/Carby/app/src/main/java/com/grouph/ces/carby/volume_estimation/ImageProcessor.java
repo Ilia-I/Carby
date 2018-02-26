d2de040bb5268a8f9cbed2087a1f63a79263bf3f
Ilia <ilia.iliev.2013@uni.strath.ac.uk>
2018-02-26 18:26:43 +0000

    reference object detection worksgit add --all! The result is an image that contains both the reference object the the result from grabcut.

--- app/Carby/app/src/main/java/com/grouph/ces/carby/volume_estimation/ImageProcessor.java
+++ app/Carby/app/src/main/java/com/grouph/ces/carby/volume_estimation/ImageProcessor.java
@@ -19,7 +19,6 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.Features2d;
 import org.opencv.imgproc.Imgproc;
@@ -35,6 +34,7 @@ import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 public class ImageProcessor {
 
     private static String TAG = "ImageProcessor";
+    private Scalar white = new Scalar(255,255,255);
 
     public Bitmap performGrabCut(Bitmap input) {
         File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
@@ -49,18 +49,17 @@ public class ImageProcessor {
         // Scale to 640x480
         Bitmap scaledPicture = Bitmap.createScaledBitmap(picture32, 640, 480, false);
 
-        // Convert to OpenCV mat
-        Mat mat = new Mat(640,480, CvType.CV_8UC3);
+        // Convert to OpenCV origImage
+        Mat origImage = new Mat(640,480, CvType.CV_8UC3);
 
-        // Place bitmap in mat
-        Utils.bitmapToMat(scaledPicture, mat);
+        // Place bitmap in origImage
+        Utils.bitmapToMat(scaledPicture, origImage);
 
         // Remove alpha channels from bitmap
-        Imgproc.cvtColor(mat,mat,Imgproc.COLOR_RGBA2RGB);
+        Imgproc.cvtColor(origImage,origImage,Imgproc.COLOR_RGBA2RGB);
 
         // Initialise models, masks, foreground and background
-        Mat background = new Mat(mat.size(), CvType.CV_8UC3,
-                new Scalar(255, 255, 255));
+        Mat background = new Mat(origImage.size(), CvType.CV_8UC3, white);
         Mat firstMask = new Mat();
         Mat bgModel = new Mat();
         Mat fgModel = new Mat();
@@ -68,33 +67,31 @@ public class ImageProcessor {
         Mat source = new Mat(1, 1, CvType.CV_8U, new Scalar(Imgproc.GC_PR_FGD));
         Mat dst = new Mat();
 
-
         // Create bounding box
-        Point p1 = new Point((mat.size().width-250)/2,(mat.size().height-250)/2);
-        Point p2 = new Point((mat.size().width+250)/2, (mat.size().height+250)/2);
+        Point p1 = new Point((origImage.size().width-250)/2,(origImage.size().height-250)/2);
+        Point p2 = new Point((origImage.size().width+250)/2, (origImage.size().height+250)/2);
         Rect rect = new Rect(p1, p2);
 
-        Imgproc.grabCut(mat, firstMask, rect, bgModel, fgModel,
+        Imgproc.grabCut(origImage, firstMask, rect, bgModel, fgModel,
                 5, Imgproc.GC_INIT_WITH_RECT);
 
         Core.compare(firstMask, source, firstMask, Core.CMP_EQ);
 
-        Mat foreground = new Mat(mat.size(), CvType.CV_8UC3,
-                new Scalar(255, 255, 255));
-        mat.copyTo(foreground, firstMask);
+        Mat foreground = new Mat(origImage.size(), CvType.CV_8UC3, white);
+        origImage.copyTo(foreground, firstMask);
 
-        Scalar color = new Scalar(255, 0, 0, 255);
-        Imgproc.rectangle(mat, p1, p2, color);
+        //uncomment to draw the bounding box
+        /*Scalar color = new Scalar(255, 0, 0, 255);
+        Imgproc.rectangle(origImage, p1, p2, color);
+        */
 
         Mat vals = new Mat(1, 1, CvType.CV_8UC3, new Scalar(0.0));
         Mat tmp = new Mat();
 
-        Imgproc.resize(background, tmp, mat.size());
+        Imgproc.resize(background, tmp, origImage.size());
         background = tmp;
-        mask = new Mat(foreground.size(), CvType.CV_8UC1,
-                new Scalar(255, 255, 255));
 
-        Imgproc.cvtColor(foreground, mask, Imgproc.COLOR_BGR2GRAY);
+        mask = grayImg(foreground);
         Imgproc.threshold(mask, mask, 254, 255, Imgproc.THRESH_BINARY_INV);
 
         background.copyTo(dst);
@@ -107,57 +104,49 @@ public class ImageProcessor {
         fgModel.release();
         vals.release();
 
+        Mat refObjMat =findRectangle(origImage);
+        origImage.copyTo(dst,refObjMat);
         dst=featureDetect(dst);
-        
-        try {
-            mat=findRectangle(mat);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
 
         //convert back to bitmap
-        Utils.matToBitmap(mat, scaledPicture);
+        Utils.matToBitmap(dst, scaledPicture);
         return scaledPicture;
     }
 
-    private Mat featureDetect(Mat img){
-        Mat blurredImage = new Mat();
-        Imgproc.blur(img, blurredImage, new Size(7, 7));
-
-        //convert to gray
+    private Mat grayImg(Mat img){
         Mat gray = new Mat(img.width(), img.height(), CvType.CV_8U, new Scalar(4));
         Imgproc.cvtColor(img, gray, Imgproc.COLOR_BGR2GRAY);
+        return gray;
+    }
+
+    private Mat featureDetect(Mat img){
+        //convert to gray
+        Mat gray = grayImg(img);
 
         FeatureDetector fd = FeatureDetector.create(FeatureDetector.FAST);
         MatOfKeyPoint regions = new MatOfKeyPoint();
         fd.detect(gray, regions);
 
-        Mat output=new Mat();
-        //int r=regions.rows();
-        //System.out.println("REGIONS ARE: " + regions);
-        Features2d.drawKeypoints(img, regions,output );
+        Features2d.drawKeypoints(img, regions,img );
 
-        return output;
+        return img;
     }
 
 
 
-    private Mat findRectangle(Mat src) throws Exception {
+    private Mat findRectangle(Mat src) {
         Mat blurred = src.clone();
         Imgproc.medianBlur(src, blurred, 9);
 
         Mat gray0 = new Mat(blurred.size(), CvType.CV_8U), gray = new Mat();
 
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-
-        List<Mat> blurredChannel = new ArrayList<Mat>();
+        List<MatOfPoint> contours = new ArrayList<>();
+        List<Mat> blurredChannel = new ArrayList<>();
         blurredChannel.add(blurred);
-        List<Mat> gray0Channel = new ArrayList<Mat>();
+        List<Mat> gray0Channel = new ArrayList<>();
         gray0Channel.add(gray0);
 
         MatOfPoint2f approxCurve;
-
         double maxArea = 0;
         int maxId = -1;
 
@@ -168,9 +157,8 @@ public class ImageProcessor {
             int thresholdLevel = 1;
             for (int t = 0; t < thresholdLevel; t++) {
                 if (t == 0) {
-                    Imgproc.Canny(gray0, gray, 10, 20, 3, true); // true ?
-                    Imgproc.dilate(gray, gray, new Mat(), new Point(-1, -1), 1); // 1
-                    // ?
+                    Imgproc.Canny(gray0, gray, 10, 20, 3, true);
+                    Imgproc.dilate(gray, gray, new Mat(), new Point(-1, -1), 1);
                 } else {
                     Imgproc.adaptiveThreshold(gray0, gray, thresholdLevel,
                             Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -208,13 +196,15 @@ public class ImageProcessor {
                 }
             }
         }
-
+        Mat mask = new Mat(src.size(), CvType.CV_8UC3,
+                new Scalar(0, 0, 0));
+        Mat crop=new Mat(src.size(), CvType.CV_8UC3, white);
         if (maxId >= 0) {
-            Imgproc.drawContours(src, contours, maxId, new Scalar(255, 0, 0,
-                    .8), 8);
+            Imgproc.drawContours(mask, contours, maxId, white, -1);
+
 
         }
-        return src;
+        return mask;
     }
 
 
