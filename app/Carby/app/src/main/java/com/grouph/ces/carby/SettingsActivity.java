9ed28a7a380c285a022f165a4c2e59c720a8061b
George Cassels <george.cassels@gmail.com>
2018-03-18 13:27:51 +0000

    added back menu to settings action bar

--- app/Carby/app/src/main/java/com/grouph/ces/carby/SettingsActivity.java
+++ app/Carby/app/src/main/java/com/grouph/ces/carby/SettingsActivity.java
@@ -1,12 +1,13 @@
 package com.grouph.ces.carby;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +17,14 @@ public class SettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
     }
 }
