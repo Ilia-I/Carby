8601e07a56aa257291a506d09444e31b611208cb
George Cassels <george.cassels@gmail.com>
2018-03-18 13:52:00 +0000

    moved camera calibration to settings

--- app/Carby/app/src/main/java/com/grouph/ces/carby/BaseActivity.java
+++ app/Carby/app/src/main/java/com/grouph/ces/carby/BaseActivity.java
@@ -40,10 +40,10 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_calibration) {
+        /* if (id == R.id.nav_calibration) {
             Intent camCal = new Intent(this, CameraCalibrationActivity.class);
             startActivity(camCal);
-        } /*else if (id == R.id.nav_feature) {
+        } else if (id == R.id.nav_feature) {
 
         }*/
 
